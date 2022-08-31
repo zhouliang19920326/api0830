389bc2a4d6928ba5928eb7cf9776eb369eff69d2
想流浪的鱼 <v_vyqlzhou@tencent.com>
2022-08-31 17:29:01 +0800

    更新构建文件

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 echo "运行容器python执行自动化" 
-docker run --rm -w=$WORKSPACE --volumes-from=jenkins python3.9:zhouliang 
+docker run --rm -w=$WORKSPACE --volumes-from=jenkins_save01 python3.9:zhouliang 
 echo "python执行自动化执行成功"
